da32241a5ae3d9a06bb024c93cd6adf54f0e4a9a
Yuki Itoh <yukiitohand@gmail.com>
2020-12-01 19:48:17 -0500

    bugs are fixed and a new model is added

--- CAHVOR_MODEL.m
+++ CAHVOR_MODEL.m
@@ -1,26 +1,19 @@
-classdef CAHV_MODEL < handle
-    % CAHV CAMERA model class
+classdef CAHVOR_MODEL < CAHV_MODEL
+    % CAHVOR CAMERA model class
     %  CAHVOR model information stored. You can only have CAHV. 
     %  Properties
-    %   C, A, H, V: model components. 1x3 vector.
+    %   C, A, H, V, O, R: model components. 1x3 vector.
+    %   type: type of the model {'CAHV','CAHVOR'}
     
     properties
-        type   %'CAHV'
-        C
-        A
-        H
-        V
-        hc
-        vc
-        hs
-        vs
-        Hdash
-        Vdash
+        O
+        R
     end
     
     methods
-        function obj = CAHV_MODEL(varargin)
-            obj.C = []; obj.A = []; obj.H = []; obj.V = [];
+        function obj = CAHVOR_MODEL(varargin)
+            obj.C = []; obj.A = []; obj.H = []; obj.V = []; obj.O = [];
+            obj.R = [];
             if (rem(length(varargin),2)==1)
                 error('Optional parameters should always go by pairs');
             else
@@ -34,18 +27,34 @@ classdef CAHV_MODEL < handle
                             obj.H = reshape(varargin{i+1},1,[]);
                         case {'V','VERTICAL'}
                             obj.V = reshape(varargin{i+1},1,[]);
+                        case {'O','OPTICAL'}
+                            if ~isempty(varargin{i+1})
+                                obj.O = reshape(varargin{i+1},1,[]);
+                            end
+                        case {'R','RADIAL'}
+                            if ~isempty(varargin{i+1})
+                                obj.R = reshape(varargin{i+1},1,[]);
+                            end
                         otherwise
                             error('Parameter: %s', varargin{i});   
                     end
                 end
             end
-            obj.type = 'CAHV';
+            if isempty(obj.O) && isempty(obj.R)
+                obj.type = 'CAHV';
+            elseif ~isempty(obj.O) && ~isempty(obj.R)
+                obj.type = 'CAHVOR';
+            else
+                error('Only either of O and R is given');
+            end
         end
         function tf = eq(obj1,obj2)
             if all(obj1.C == obj2.C) ...
                 && all(obj1.A == obj2.A) ...
                 && all(obj1.H == obj2.H) ...
                 && all(obj1.V == obj2.V)
+                && all(obj1.O == obj2.O) ...
+                && all(obj1.R == obj2.R)
                 tf = true;
             else
                 tf = false;
@@ -56,6 +65,8 @@ classdef CAHV_MODEL < handle
                 || (all(obj1.A ~= obj2.A)) ...
                 || (all(obj1.H ~= obj2.H)) ...
                 || (all(obj1.V ~= obj2.V))
+                || (all(obj1.O ~= obj2.O)) ...
+                || (all(obj1.R ~= obj2.R))
                 tf = true;
             else
                 tf = false;

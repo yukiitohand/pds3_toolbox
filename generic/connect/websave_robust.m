5453fdc40cde26fade43844740564456f85f5993
Yuki Itoh <yukiitohand@gmail.com>
2021-05-25 14:53:24 -0400

    websave_robust.m is fixed for resuming

--- generic/connect/websave_robust.m
+++ generic/connect/websave_robust.m
@@ -1,7 +1,7 @@
 function websave_robust(filename,url,options)
 % websave_robust(filename,url,options)
-%  robust version of websave. If the downloading stops, restart where it
-%  stops.
+%  robust version of websave. If the downloading stops with an error, 
+%  resume where it stops.
 %  Input
 %   filename: local file url
 %   url:  remote url
@@ -9,21 +9,48 @@ function websave_robust(filename,url,options)
 
 options_tmp = options;
 
-flg_fin = 0;
+flg_fin   = 0;
+n_failure = 0;
+max_trial = 5;
 
-while ~flg_fin
+filename_ori = filename;
+
+while ~flg_fin || n_failure < max_trial
     try
         websave(filename,url,options_tmp);
         flg_fin = 1;
     catch
         file_info = dir(filename);
         file_size_cur = file_info.bytes;
-        fprintf('download stopped with an error at bytes=%d\n',file_size_cur);
         range_val = sprintf('bytes=%d-',file_size_cur);
         % resume_byte_pos
         options_tmp.HeaderFields = {'Range',range_val };
-        websave(filename,url,options_tmp);
+        fprintf('download stopped with an error at bytes=%d. Resuming ...\n',file_size_cur);
+        
+        % Change the file name and resume downloading from the bytes
+        % specified by the Range field.
+        % saving to another file.
+        n_failure = n_failure + 1;
+        filename = sprintf('%s-%02d',filename_ori,n_failure);
     end
+end
+
+if n_failure>0
+    % If you experienced several failures in downloading, combine them at
+    % the end.
+    fid = fopen(filename_ori,'a+');
+    for n=1:n_failure
+        filename = sprintf('%s-%02d',filename_ori,n);
+        fidn = fopen(filename,'r');
+        fwrite(fid,fread(fidn));
+        fclose(fidn);
+        delete(filename);
+    end
+    fclose(fid);
+end
+
+if n_failure >= max_trial
+    error('Maximum number of trials is reached. Download failed.');
 end
 
 end

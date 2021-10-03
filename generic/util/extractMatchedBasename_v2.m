0c21ecb953c903433428a59e96dac049dc043fb4
Yuki Itoh <yukiitohand@gmail.com>
2021-10-02 22:10:30 -0400

    extractMatchedBasename_v2.m is copied from crism_toolbox

---
+++ generic/util/extractMatchedBasename_v2.m
@@ -0,0 +1,53 @@
+function [basename,fname_wext] = extractMatchedBasename_v2(basenamePtr,fnamelist,varargin)
+% [basename] = extractMatchedBasename_v2(basenamePtr,fnamelist,varargin)
+%   match the pattern to filenames in fnamelist
+%
+%  INPUTS
+%    basenamePtr: pattern input for regexpi
+%    fnamelist  : cell array of filenames (with or without extensions)
+%  OUTPUTS
+%    basename: string or cell array unique list of matched basenames (no extension)
+%    fname_wext: cell array, all the file names with different extension
+%  Optional Parameters
+%      'MATCH_EXACT'    : binary, if basename match should be exact match
+%                         or not.
+%                         (default) false
+
+is_exact = false;
+if (rem(length(varargin),2)==1)
+    error('Optional parameters should always go by pairs');
+else
+    for i=1:2:(length(varargin)-1)
+        switch upper(varargin{i})
+            case 'EXACT'
+                is_exact = varargin{i+1};
+            otherwise
+                error(['Unrecognized option: ''' varargin{i} '''']);
+        end
+    end
+end
+
+
+if isempty(fnamelist)
+    basename = ''; fname_wext = [];
+else
+    if is_exact
+        basenamePtr = ['^' basenamePtr '[[.][a-zA-Z]]*$'];
+    end
+    matching = cellfun(@(x) ~isempty(regexpi(x,basenamePtr,'ONCE')),fnamelist);
+    if sum(matching)>0
+        fname_wext = fnamelist(matching);
+        basenameList = cell(1,length(fname_wext));
+        for i=1:length(fname_wext)
+            [~,basename,ext] = fileparts(fname_wext{i});
+            basenameList{i} = basename;            
+        end
+        basename = unique(basenameList);
+        if length(basename)==1
+            basename = basename{1};
+        end
+    else
+        basename = ''; fname_wext = [];
+    end
+end
+end

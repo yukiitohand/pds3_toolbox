b473834e0a73fede914354d53ec0a2196cf2259b
Yuki Itoh <yukiitohand@gmail.com>
2022-07-26 11:54:58 -0400

    a minor change

--- generic/util/extractMatchedBasename_v2.m
+++ generic/util/extractMatchedBasename_v2.m
@@ -14,6 +14,7 @@ function [basename,fname_wext] = extractMatchedBasename_v2(basenamePtr,fnamelist
 %                         (default) false
 
 is_exact = false;
+celloutput = false;
 if (rem(length(varargin),2)==1)
     error('Optional parameters should always go by pairs');
 else
@@ -21,20 +22,22 @@ else
         switch upper(varargin{i})
             case 'EXACT'
                 is_exact = varargin{i+1};
+            case 'CELLOUTPUT'
+                celloutput = varargin{i+1};
             otherwise
-                error(['Unrecognized option: ''' varargin{i} '''']);
+                error('Unrecognized option: %s', varargin{i});
         end
     end
 end
 
 
 if isempty(fnamelist)
-    basename = ''; fname_wext = [];
+    basename = {}; fname_wext = {};
 else
     if is_exact
         basenamePtr = ['^' basenamePtr '[[.][a-zA-Z]]*$'];
     end
-    matching = cellfun(@(x) ~isempty(regexpi(x,basenamePtr,'ONCE')),fnamelist);
+    matching = ~cellfun('isempty',regexpi(fnamelist,basenamePtr,'ONCE'));
     if sum(matching)>0
         fname_wext = fnamelist(matching);
         basenameList = cell(1,length(fname_wext));
@@ -43,11 +46,11 @@ else
             basenameList{i} = basename;            
         end
         basename = unique(basenameList);
-        if length(basename)==1
+        if ~celloutput && length(basename)==1
             basename = basename{1};
         end
     else
-        basename = ''; fname_wext = [];
+        basename = {}; fname_wext = {};
     end
 end
 end

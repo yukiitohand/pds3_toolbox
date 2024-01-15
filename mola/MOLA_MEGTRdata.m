1d9c23d1b4dd0068a32705af157783a3f1efa839
Yuki Itoh <yukiitohand@gmail.com>
2024-01-15 07:30:56 -0500

    subimage reader is updated

--- mola/MOLA_MEGTRdata.m
+++ mola/MOLA_MEGTRdata.m
@@ -29,8 +29,27 @@ classdef MOLA_MEGTRdata < ENVIRasterSingleLayerEquirectProjRot0
         end
         
         function [subimg] = get_subimage_wPixelRange(obj,xrange,yrange,varargin)
+            compensate_offset = false;
+            varargin_retainIdxBool = true(1,numel(varargin));
+            if (rem(numel(varargin),2)==1)
+                error('Optional parameters should always go by pairs');
+            else
+                for i=1:2:(length(varargin)-1)
+                    switch upper(varargin{i})
+                        case 'COMPENSATE_OFFSET'
+                            compensate_offset = varargin{i+1};
+                            varargin_retainIdxBool([i i+1]) = false;
+                        case {'PRECISION','REPLACE_DATA_IGNORE_VALUE','REPVAL_DATA_IGNORE_VALUE'}
+                        otherwise
+                            error('Unrecognized option: %s',varargin{i});
+                    end
+                end
+            end
             [subimg] = get_subimage_wPixelRange@ENVIRasterSingleLayerEquirectProjRot0(...
-                obj,xrange,yrange,'Precision','int16',varargin{:});
+                obj,xrange,yrange,'Precision','int16',varargin{varargin_retainIdxBool});
+            if compensate_offset && isfield(obj.lbl.OBJECT_IMAGE,'OFFSET')
+                subimg = subimg + obj.lbl.OBJECT_IMAGE.OFFSET;
+            end
         end
         
         
